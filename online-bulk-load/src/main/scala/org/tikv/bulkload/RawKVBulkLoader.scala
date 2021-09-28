70da45bf1faf0a466dfc28855434e3d058472fbb
Peng Guanwen <pg999w@outlook.com>
2021-09-28 11:58:21 +0800

    Pause merge when importing (#32)
    Signed-off-by: Peng Guanwen <pg999w@outlook.com>

--- online-bulk-load/src/main/scala/org/tikv/bulkload/RawKVBulkLoader.scala
+++ online-bulk-load/src/main/scala/org/tikv/bulkload/RawKVBulkLoader.scala
@@ -32,7 +32,7 @@ import org.tikv.common.util.{
   FastByteComparisons,
   Pair
 }
-import org.tikv.common.{TiConfiguration, TiSession}
+import org.tikv.common.{PDChecker, TiConfiguration, TiSession}
 import org.tikv.shade.com.google.protobuf.ByteString
 
 import scala.collection.JavaConverters._
@@ -82,8 +82,9 @@ class RawKVBulkLoader(tiConf: TiConfiguration, sparkConf: SparkConf) extends Ser
     val orderedSplitPoints = getRegionSplitPoints(rdd)
 
     // switch to normal mode
+    val pdClient = tiSession.getPDClient
     val switchTiKVModeClient =
-      new SwitchTiKVModeClient(tiSession.getPDClient, tiSession.getImporterRegionStoreClientBuilder)
+      new SwitchTiKVModeClient(pdClient, tiSession.getImporterRegionStoreClientBuilder)
     switchTiKVModeClient.switchTiKVToNormalMode()
 
     // call region split and scatter
@@ -97,7 +98,8 @@ class RawKVBulkLoader(tiConf: TiConfiguration, sparkConf: SparkConf) extends Ser
       case e: Throwable => logger.warn("error during splitRegionAndScatter!", e)
     }
 
-    // switch to import mode
+    // pause merge and switch to import mode
+    pdClient.keepPauseChecker(PDChecker.Merge)
     switchTiKVModeClient.keepTiKVToImportMode()
 
     // refetch region info
@@ -114,6 +116,8 @@ class RawKVBulkLoader(tiConf: TiConfiguration, sparkConf: SparkConf) extends Ser
     finalRDD.foreachPartition { itor =>
       writeAndIngest(itor.map(pair => (pair._1, pair._2)), partitioner)
     }
+    pdClient.stopKeepPauseChecker(PDChecker.Merge)
+    pdClient.resumeChecker(PDChecker.Merge)
     switchTiKVModeClient.stopKeepTiKVToImportMode()
     switchTiKVModeClient.switchTiKVToNormalMode()
     logger.info("finish to load data.")
