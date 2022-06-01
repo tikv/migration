8f9c3f3d4d19cfec74ec78aa721c8de73b4c34b9
haojinming <jinming.hao@pingcap.com>
2022-06-01 10:55:30 +0800

    improve restore concurrency
    Signed-off-by: haojinming <jinming.hao@pingcap.com>

--- br/pkg/task/common.go
+++ br/pkg/task/common.go
@@ -66,7 +66,7 @@ const (
 	defaultSwitchInterval       = 5 * time.Minute
 	defaultGRPCKeepaliveTime    = 10 * time.Second
 	defaultGRPCKeepaliveTimeout = 3 * time.Second
-	defaultChecksumConcurrency  = 128
+	defaultChecksumConcurrency  = 512
 
 	flagCipherType    = "crypter.method"
 	flagCipherKey     = "crypter.key"
