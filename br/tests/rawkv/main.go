1d221a4ed0e9980e8f11e31700714bb4afdb383c
haojinming <jinming.hao@pingcap.com>
2022-07-26 10:44:11 +0800

    add tikv v2 br test
    Signed-off-by: haojinming <jinming.hao@pingcap.com>

---
+++ br/tests/rawkv/main.go
@@ -0,0 +1,297 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	units "github.com/docker/go-units"
+	"github.com/juju/errors"
+	"github.com/pingcap/kvproto/pkg/kvrpcpb"
+	"github.com/pingcap/log"
+	"github.com/tikv/client-go/v2/oracle"
+	"github.com/tikv/client-go/v2/rawkv"
+	pd "github.com/tikv/pd/client"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+var (
+	maxMsgSize   = int(128 * units.MiB) // pd.ScanRegion may return a large response
+	maxBatchSize = uint(1024)           // max batch size with BatchPut
+
+	pdAddr        = flag.String("pd", "127.0.0.1:2379", "Address of PD")
+	apiVersionInt = flag.Uint("api-version", 1, "Api version of tikv-server")
+	br            = flag.String("br", "br", "The br binary to be tested.")
+	brStorage     = flag.String("br-storage", "local:///tmp/backup_restore_test", "The url to store SST files of backup/resotre. Default: 'local:///tmp/backup_restore_test'")
+)
+
+type RawKVBRTester struct {
+	pdAddr      string
+	apiVersion  kvrpcpb.APIVersion
+	br          string
+	brStorage   string
+	rawkvClient *rawkv.Client
+	pdClient    pd.Client
+}
+
+func NewPDClient(ctx context.Context, pdAddrs string) (pd.Client, error) {
+	addrs := strings.Split(pdAddrs, ",")
+	maxCallMsgSize := []grpc.DialOption{
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(maxMsgSize)),
+		grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(maxMsgSize)),
+	}
+	return pd.NewClientWithContext(
+		ctx, addrs, pd.SecurityOption{},
+		pd.WithGRPCDialOptions(maxCallMsgSize...),
+		pd.WithCustomTimeoutOption(10*time.Second),
+		pd.WithMaxErrorRetry(3),
+	)
+}
+
+func NewRawKVBRTester(ctx context.Context, pd, br, storage string, version kvrpcpb.APIVersion) (*RawKVBRTester, error) {
+	cli, err := rawkv.NewClientWithOpts(context.TODO(), []string{pd},
+		rawkv.WithAPIVersion(version))
+	if err != nil {
+		fmt.Println("fail to new rawkv client", err)
+		return nil, err
+	}
+	pdClient, err := NewPDClient(ctx, pd)
+	if err != nil {
+		fmt.Println("fail to new pd client", err)
+		return nil, err
+	}
+	return &RawKVBRTester{
+		pdAddr:      pd,
+		br:          br,
+		apiVersion:  version,
+		brStorage:   storage,
+		rawkvClient: cli,
+		pdClient:    pdClient,
+	}, nil
+}
+
+func (t *RawKVBRTester) Close() {
+	if t.rawkvClient != nil {
+		t.rawkvClient.Close()
+	}
+	t.pdClient.Close()
+}
+
+func min(left, right uint) uint {
+	if left < right {
+		return left
+	} else {
+		return right
+	}
+}
+
+func GenerateTestData(keyIndex uint, prefix []byte) (key, value []byte) {
+	key = []byte(fmt.Sprintf("%s:_%019d", string(prefix), keyIndex))
+	value = []byte(fmt.Sprintf("v0%020d", keyIndex))
+	return key, value
+}
+
+func BatchGenerateData(keyIndex uint, keyCnt uint, prefix []byte) (keys, values [][]byte) {
+	keys = make([][]byte, 0, keyCnt)
+	values = make([][]byte, 0, keyCnt)
+	for idx := keyIndex; idx < keyIndex+keyCnt; idx++ {
+		key, value := GenerateTestData(idx, prefix)
+		keys = append(keys, key)
+		values = append(values, value)
+	}
+	return keys, values
+}
+
+// TODO: make it concurrency for large scale data.
+func (t *RawKVBRTester) PreloadData(ctx context.Context, keyCnt uint, prefix []byte) error {
+	startIdx := uint(0)
+	for startIdx < keyCnt {
+		batchCnt := min(maxBatchSize, keyCnt-startIdx)
+		keys, values := BatchGenerateData(startIdx, batchCnt, prefix)
+		err := t.rawkvClient.BatchPut(ctx, keys, values)
+		if err != nil {
+			return errors.Trace(err)
+		}
+		startIdx += batchCnt
+	}
+	return nil
+}
+
+func (t *RawKVBRTester) CleanData(ctx context.Context, prefix []byte) error {
+	return t.rawkvClient.DeleteRange(ctx, prefix, append(prefix, 0))
+}
+
+func (t *RawKVBRTester) Checksum(ctx context.Context) (rawkv.RawChecksum, error) {
+	return t.rawkvClient.Checksum(ctx, []byte{}, []byte{})
+}
+
+func (t *RawKVBRTester) Backup(ctx context.Context, dstAPIVersion kvrpcpb.APIVersion, safeInterval int64) ([]byte, error) {
+	brCmd := NewTiKVBrCmd("backup raw")
+	brCmdStr := brCmd.Pd(t.pdAddr).
+		Storage(t.brStorage, true).
+		CheckReq(false).
+		DstApiVersion(dstAPIVersion.String()).
+		SafeInterval(safeInterval).
+		Checksum(true).
+		Build()
+	return t.ExecBRCmd(ctx, brCmdStr)
+}
+
+func (t *RawKVBRTester) Restore(ctx context.Context) ([]byte, error) {
+	brCmd := NewTiKVBrCmd("restore raw")
+	brCmdStr := brCmd.Pd(t.pdAddr).
+		Storage(t.brStorage, true).
+		CheckReq(false).
+		Checksum(true).
+		Build()
+	return t.ExecBRCmd(ctx, brCmdStr)
+}
+
+func (t *RawKVBRTester) InjectFailpoint(failpoint string) error {
+	return os.Setenv("GO_FAILPOINTS", failpoint)
+}
+
+func (t *RawKVBRTester) ExecBRCmd(ctx context.Context, cmdStr string) ([]byte, error) {
+	cmd := exec.CommandContext(ctx, t.br, strings.Split(cmdStr, " ")...)
+	return cmd.Output()
+}
+
+func (t *RawKVBRTester) ClearStorage() error {
+	return os.RemoveAll(t.brStorage)
+}
+
+func (t *RawKVBRTester) GetTso(ctx context.Context) (uint64, error) {
+	p, l, err := t.pdClient.GetTS(ctx)
+	if err != nil {
+		return 0, err
+	}
+	return oracle.ComposeTS(p, l), nil
+}
+
+func ParseBackupTSFromOutput(output []byte) uint64 {
+	flysnowRegexp := regexp.MustCompile(`BackupTS=([0-9]*)]`)
+	if flysnowRegexp == nil {
+		log.Panic("regex error")
+	}
+	params := flysnowRegexp.FindStringSubmatch(string(output))
+
+	log.Info("backup output", zap.ByteString("output", output), zap.Int("match len", len(params)))
+	for _, param := range params {
+		log.Info("regex output", zap.String("regex output", param))
+	}
+	backupTs, err := strconv.ParseUint(params[len(params)-1], 10, 64)
+	if err != nil {
+		log.Panic("parse backup ts fails", zap.String("ts", params[len(params)-1]))
+	}
+	log.Info("get backup ts", zap.Uint64("ts", backupTs))
+	return backupTs
+}
+
+func CheckBackupTS(apiVersion kvrpcpb.APIVersion, tso uint64, backupOutput []byte, safeInterval int64) {
+	if apiVersion != kvrpcpb.APIVersion_V2 {
+		return
+	}
+	backupTS := ParseBackupTSFromOutput(backupOutput)
+	tsoPhysical := oracle.ExtractPhysical(tso)
+	backupTSPhysical := oracle.ExtractPhysical(backupTS)
+	diff := (tsoPhysical - backupTSPhysical) / 1000
+	if math.Abs(float64(diff-safeInterval)) > 1.0 {
+		log.Panic("backup ts does not match the rule", zap.Int64("backupTSPhysical", backupTSPhysical),
+			zap.Int64("tsoPhysical", tsoPhysical), zap.Int64("safeInterval", safeInterval))
+	}
+}
+
+func runTestWithFailPoint(failpoint string) {
+	apiVersion := kvrpcpb.APIVersion_V1TTL
+	if *apiVersionInt == 2 {
+		apiVersion = kvrpcpb.APIVersion_V2
+	}
+	ctx := context.TODO()
+
+	fmt.Println("test api version", apiVersion)
+
+	tester, err := NewRawKVBRTester(ctx, *pdAddr, *br, *brStorage, apiVersion)
+	if err != nil {
+		log.Panic("New Tester Error", zap.Error(err))
+	}
+	err = tester.InjectFailpoint(failpoint)
+	if err != nil {
+		log.Panic("Inject failpoint Error", zap.Error(err))
+	}
+
+	if err := tester.ClearStorage(); err != nil {
+		log.Panic("ClearStorage fail", zap.Error(err))
+	}
+
+	prefix := []byte{'a'}
+	err = tester.PreloadData(ctx, 1000, prefix)
+	if err != nil {
+		log.Panic("Preload data fail", zap.Error(err))
+	}
+
+	oriChecksum, err := tester.Checksum(ctx)
+	if err != nil {
+		log.Panic("Checksum fail", zap.Error(err))
+	}
+
+	tso := uint64(0)
+	if apiVersion == kvrpcpb.APIVersion_V2 {
+		tso, err = tester.GetTso(ctx)
+		if err != nil {
+			log.Panic("Get tso fail", zap.Error(err))
+		}
+	}
+	safeInterval := int64(120) // 2m
+	backupOutput, err := tester.Backup(ctx, apiVersion, safeInterval)
+	if err != nil {
+		log.Panic("Backup fail", zap.Error(err), zap.ByteString("output", backupOutput))
+	}
+	log.Info("backup finish:", zap.ByteString("output", backupOutput))
+	CheckBackupTS(apiVersion, tso, backupOutput, safeInterval)
+
+	err = tester.CleanData(ctx, prefix)
+	if err != nil {
+		log.Panic("Clean data fail", zap.Error(err))
+	}
+	restoreOutput, err := tester.Restore(ctx)
+	if err != nil {
+		log.Panic("Restore fail", zap.Error(err), zap.ByteString("output", restoreOutput))
+	}
+	log.Info("Restore finish:", zap.ByteString("output", restoreOutput))
+	restoreChecksum, err := tester.Checksum(ctx)
+	if err != nil {
+		log.Panic("Checksum fail", zap.Error(err))
+	}
+	if oriChecksum != restoreChecksum {
+		log.Panic("Checksum mismatch", zap.Reflect("OriChecksum", oriChecksum), zap.Reflect("RestoreChecksum", restoreChecksum))
+	}
+
+	if apiVersion == kvrpcpb.APIVersion_V1TTL {
+		if err := tester.ClearStorage(); err != nil {
+			log.Panic("ClearStorage fail", zap.Error(err))
+		}
+		backupOutput, err := tester.Backup(ctx, kvrpcpb.APIVersion_V2, safeInterval)
+		if err != nil {
+			log.Panic("Backup fail", zap.Error(err), zap.ByteString("output", backupOutput))
+		}
+		log.Info("backup conversion finish:", zap.ByteString("output", backupOutput))
+	}
+}
+
+func main() {
+	flag.Parse()
+	failpoints := []string{"",
+		"github.com/tikv/migration/br/pkg/backup/tikv-region-error=return(\"region error\")",
+	}
+	for _, failpoint := range failpoints {
+		runTestWithFailPoint(failpoint)
+	}
+}
--- br/tests/rawkv/run.py
+++ br/tests/rawkv/run.py
@@ -28,32 +28,33 @@ class rawkvTester:
         self.helper = global_args.helper
         self.br_storage = global_args.br_storage
         self.failpoints = failpoints
+        self.api_version = global_args.api_version
 
 
-    def test_dst_apiv1(self):
+    def test_dst_apiv1(self, test_restore):
         test_name = self.test_dst_apiv1.__name__
         storage_dir = self.br_storage + "/" + test_name
 
         self._clean(storage_dir)
-        self._run_br_test("v1", storage_dir)
+        self._run_br_test("v1", storage_dir, test_restore)
         self._success_msg(test_name)
 
 
-    def test_dst_apiv1ttl(self):
+    def test_dst_apiv1ttl(self, test_restore):
         test_name = self.test_dst_apiv1ttl.__name__
         storage_dir = self.br_storage + "/" + test_name
 
         self._clean(storage_dir)
-        self._run_br_test("v1ttl", storage_dir)
+        self._run_br_test("v1ttl", storage_dir, test_restore)
         self._success_msg(test_name)
 
 
-    def test_dst_apiv2(self):
+    def test_dst_apiv2(self, test_restore):
         test_name = self.test_dst_apiv2.__name__
         storage_dir = self.br_storage + "/" + test_name
 
         self._clean(storage_dir)
-        self._run_br_test("v2", storage_dir)
+        self._run_br_test("v2", storage_dir, test_restore)
         self._success_msg(test_name)
 
     def _clean(self, storage_dir):
@@ -67,7 +68,7 @@ class rawkvTester:
         print(f"PASSED: {case_name}")
 
 
-    def _run_br_test(self, dst_api_version, storage_dir):
+    def _run_br_test(self, dst_api_version, storage_dir, test_restore):
         outer_start, outer_end = "31", "3130303030303030"
         inner_start, inner_end = "311111", "311122"
 
@@ -77,34 +78,41 @@ class rawkvTester:
 
         # prepare and backup data
         self._randgen(outer_start, outer_end)
-        self._run_cmd(self.helper, "-pd", self.pd, "-mode", "put",
+        self._run_cmd(self.helper, "-pd", self.pd, "-mode", "put", "-api-version", self.api_version,
                 "-put-data", "311121:31, 31112100:32, 311122:33, 31112200:34, 3111220000:35, 311123:36")
-        self._backup_range(outer_start, outer_end, dst_api_version, storage_dir)
+        output = self._backup_range(outer_start, outer_end, dst_api_version, storage_dir)
+        print(output)
+        matched = re.search("BackupTS=.*", output)
+        if matched:
+            print("found BackupTS")
+            print(matched)
+            print(str(matched.group(0))[len("BackupTS="):])
+        else:
+            print("Not found BackupTS")
+
         cs_outer_origin = self._get_checksum(outer_start, outer_end)
         cs_inner_origin = self._get_checksum(inner_start, inner_end)
 
-        # clean and restore outer range
-        self._clean_range(outer_start, outer_end)
-        cs_outer_clean = self._get_checksum(outer_start, outer_end)
-        self._assert("clean range failed, checksum mismatch.\n  actual: {}\n  expect: {}", cs_outer_clean, cs_outer_empty)
-        self._restore_range(outer_start, outer_end, dst_api_version, storage_dir)
-        cs_outer_restore = self._get_checksum(outer_start, outer_end)
-        self._assert("restore failed, checksum mismatch.\n  actual: {}\n  expect: {}", cs_outer_restore, cs_outer_origin)
+        if test_restore:
+            # clean and restore outer range
+            self._clean_range(outer_start, outer_end)
+            cs_outer_clean = self._get_checksum(outer_start, outer_end)
+            self._assert("clean range failed, checksum mismatch.\n  actual: {}\n  expect: {}", cs_outer_clean, cs_outer_empty)
+            self._restore_range(outer_start, outer_end, dst_api_version, storage_dir)
+            cs_outer_restore = self._get_checksum(outer_start, outer_end)
+            self._assert("restore failed, checksum mismatch.\n  actual: {}\n  expect: {}", cs_outer_restore, cs_outer_origin)
 
-        # clean and restore inner range
-        self._clean_range(outer_start, outer_end)
-        cs_outer_clean = self._get_checksum(outer_start, outer_end)
-        self._assert("clean range failed, checksum mismatch.\n  actual: {}\n  expect: {}", cs_outer_clean, cs_outer_empty)
-        #self._restore_range(inner_start, inner_end, dst_api_version, storage_dir)
-        #cs_inner_restore = self._get_checksum(inner_start, inner_end)
-        #self._assert("restore failed, checksum mismatch.\n  actual: {}\n  expect: {}", cs_inner_restore, cs_inner_origin)
+            # clean and restore inner range
+            self._clean_range(outer_start, outer_end)
+            cs_outer_clean = self._get_checksum(outer_start, outer_end)
+            self._assert("clean range failed, checksum mismatch.\n  actual: {}\n  expect: {}", cs_outer_clean, cs_outer_empty)
 
 
     def _backup_range(self, start_key, end_key, dst_api_version, storage_dir):
         env = {
             'GO_FAILPOINTS': self.failpoints,
         }
-        self._run_cmd(self.br, "--pd", self.pd, "backup", "raw", "-s", storage_dir,
+        return self._run_cmd(self.br, "--pd", self.pd, "backup", "raw", "-s", storage_dir,
                 "--start", start_key, "--end", end_key, "--format", "hex", "--dst-api-version", dst_api_version,
                 "--check-requirements=false", "-L", "debug", **env)
 
@@ -119,15 +127,21 @@ class rawkvTester:
 
 
     def _randgen(self, start_key, end_key):
-        self._run_cmd(self.helper, "-pd", self.pd, "-mode", "rand-gen", "-start-key", start_key, "-end-key", end_key, "-duration", "10")
+        self._run_cmd(self.helper, "-pd", self.pd, "-mode", "rand-gen",
+            "-start-key", start_key, "-end-key", end_key, "-duration", "10",
+             "-api-version", self.api_version)
 
 
     def _clean_range(self, start_key, end_key):
-        self._run_cmd(self.helper, "-pd", self.pd, "-mode", "delete", "-start-key", start_key, "-end-key", end_key)
+        self._run_cmd(self.helper, "-pd", self.pd, "-mode", "delete",
+            "-start-key", start_key, "-end-key", end_key,
+             "-api-version", self.api_version)
 
 
     def _get_checksum(self, start_key, end_key):
-        output = self._run_cmd(self.helper, "-pd", self.pd, "-mode", "checksum", "-start-key", start_key, "-end-key", end_key)
+        output = self._run_cmd(self.helper, "-pd", self.pd, "-mode", "checksum",
+            "-start-key", start_key, "-end-key", end_key,
+             "-api-version", self.api_version)
         matched = re.search("Checksum result: .*", output)
         if matched:
             return str(matched.group(0))[len("Checksum result: "):]
@@ -173,10 +187,11 @@ def main():
     args = parse_args()
     for failpoint in [''] + FAILPOINTS:
         tester = rawkvTester(args, failpoints=failpoint)
-        # tester.test_dst_apiv1() // does not support v1ttl --> v1
-        tester.test_dst_apiv1ttl()
-        # v1ttl -> v2 is not supported in current TiKV, enable this after V1/V1ttl -> V2 is enabled.
-        # tester.test_dst_apiv2()
+        if args.api_version == "1":
+            tester.test_dst_apiv1ttl(True)
+            tester.test_dst_apiv2(False) # test apiversion conversion
+        else:
+            tester.test_dst_apiv2(True)
 
 
 def parse_args():
@@ -189,6 +204,8 @@ def parse_args():
             help = "The url to store SST files of backup/resotre. Default: 'local:///tmp/backup_restore_test'")
     parser.add_argument("--test-helper", dest = "helper", default = "./rawkv",
             help = "The test helper binary to be used to populate and clean data for the TiKV cluster. Default: './rawkv'")
+    parser.add_argument("--api-version", dest = "api_version", default = "1", required = True,
+            help = "The api version of tikv-server")
     args = parser.parse_args()
     return args
 
