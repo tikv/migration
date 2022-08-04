40df7e894504738f2fa165595d57e888ec38e249
zeminzhou <zhouzemin@pingcap.com>
2022-08-04 15:40:43 +0800

    fix lint
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- cdc/cdc/model/mounter_test.go
+++ cdc/cdc/model/mounter_test.go
@@ -5,8 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -189,5 +188,4 @@ func TestComparePolymorphicEvents(t *testing.T) {
 	for _, tc := range testCases {
 		require.Equal(t, tc.expected, ComparePolymorphicEvents(tc.i, tc.j))
 	}
-
 }
