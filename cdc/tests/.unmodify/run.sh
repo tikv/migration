824a451d756820ac8061692b12ac51ff07077e72
zzm <zhouzemin@pingcap.com>
2022-09-01 19:40:07 +0800

    [to #215] Add integration test for tikv-cdc (#216)
    * fist it
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * modify ci-cdc.yml for tikv-cdc integration
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * modify ci-cdc.yml for tikv-cdc integration
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * modify ci-cdc.yml for tikv-cdc integration
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * fix make check
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * fix make check
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * fix error
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * remove integration test
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * test it
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * integration test
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * remove unused file
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * remove unused file
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * fix comment
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * fix comment
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    * fix check
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>
    
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- cdc/tests/.unmodify/run.sh
+++ cdc/tests/.unmodify/run.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-OUT_DIR=/tmp/tikv_cdc_test
+OUT_DIR=/tmp/tidb_cdc_test
 CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-export PATH=$CUR/_utils:$CUR/../../bin:$CUR/../../scripts/bin:$PATH
+export PATH=$PATH:$CUR/_utils:$CUR/../bin
 
 mkdir -p $OUT_DIR || true
 
@@ -14,15 +14,15 @@ if [ "${1-}" = '--debug' ]; then
 
 	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 
-	PATH=$CUR/_utils:$CUR/../../bin:$CUR/../../scripts/bin:$PATH \
+	PATH="$CUR/../bin:$CUR/_utils:$PATH" \
 		LD_LIBRARY_PATH="$CUR/../bin:$CUR/_utils:$PATH" \
 		OUT_DIR=$OUT_DIR \
 		TEST_NAME="debug" \
 		start_tidb_cluster --workdir $WORK_DIR
 
-	tikv-cdc server --log-file $WORK_DIR/cdc.log --log-level debug --addr 127.0.0.1:8600 >$WORK_DIR/stdout.log 2>&1 &
+	cdc server --log-file $WORK_DIR/cdc.log --log-level debug --addr 127.0.0.1:8300 >$WORK_DIR/stdout.log 2>&1 &
 	sleep 1
-	cdc cli changefeed create --sink-uri="tikv://127.0.0.1:2479"
+	cdc cli changefeed create --sink-uri="mysql://normal:123456@127.0.0.1:3306/"
 
 	echo 'You may now debug from another terminal. Press [ENTER] to exit.'
 	read line
@@ -32,7 +32,7 @@ fi
 run_case() {
 	# cleanup test binaries and data, preserve logs, if we debug one case,
 	# these files will be preserved since no more case will be run.
-	find /tmp/tikv_cdc_test/*/* -type d | xargs rm -rf || true
+	find /tmp/tidb_cdc_test/*/* -type d | xargs rm -rf || true
 	local case=$1
 	local script=$2
 	local sink_type=$3
@@ -44,10 +44,10 @@ run_case() {
 		bash "$script" "$sink_type"
 }
 
-sink_type="tikv"
+sink_type=$1
 
 set +eu
-test_case=$1
+test_case=$2
 
 if [ -z "$test_case" ]; then
 	test_case="*"
