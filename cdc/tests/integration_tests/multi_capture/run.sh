fbe16c73c1a3308c042edf312e1fc6ef9fecac05
zeminzhou <zhouzemin@pingcap.com>
2022-09-14 19:15:53 +0800

    add more integration test
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- cdc/tests/integration_tests/multi_capture/run.sh
+++ cdc/tests/integration_tests/multi_capture/run.sh
@@ -6,35 +6,41 @@ CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $CUR/../_utils/test_prepare
 WORK_DIR=$OUT_DIR/$TEST_NAME
 CDC_BINARY=tikv-cdc.test
+SINK_TYPE=$1
 UP_PD=http://$UP_PD_HOST_1:$UP_PD_PORT_1
 DOWN_PD=http://$DOWN_PD_HOST:$DOWN_PD_PORT
-SINK_TYPE=$1
 
-# This test mainly verifies kv client force reconnect can work
-# Trigger force reconnect by failpoint injection
+CDC_COUNT=3
+
 function run() {
 	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
+
 	start_tidb_cluster --workdir $WORK_DIR
+
 	cd $WORK_DIR
 
-	pd_addr="http://$UP_PD_HOST_1:$UP_PD_PORT_1"
+	# record tso before we create tables to skip the system table DDLs
+	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
+    sleep 10
+
+    rawkv_op $UP_PD put 10000
+
+	# start $CDC_COUNT cdc servers, and create a changefeed
+	for i in $(seq $CDC_COUNT); do
+		run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix "$i" --addr "127.0.0.1:860${i}"
+	done
+
 	case $SINK_TYPE in
 	tikv) SINK_URI="tikv://${DOWN_PD_HOST}:${DOWN_PD_PORT}" ;;
 	*) SINK_URI="" ;;
 	esac
 
-	# this will be triggered every 5s in kv client
-	export GO_FAILPOINTS='github.com/tikv/migration/cdc/cdc/kv/kvClientForceReconnect=return(true)'
-	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8600" --pd $pd_addr
-	changefeed_id=$(tikv-cdc cli changefeed create --pd=$pd_addr --sink-uri="$SINK_URI" 2>&1 | tail -n2 | head -n1 | awk '{print $2}')
-    sleep 10
+	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
 
-    rawkv_op $UP_PD put 10000
 	check_sync_diff $WORK_DIR $UP_PD $DOWN_PD
     rawkv_op $UP_PD delete 10000
 	check_sync_diff $WORK_DIR $UP_PD $DOWN_PD
 
-	export GO_FAILPOINTS=''
 	cleanup_process $CDC_BINARY
 }
 
