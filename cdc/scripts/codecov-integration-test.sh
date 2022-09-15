8e5b867c06bb0c372a4127fb8f67ff0f625ad8a6
zeminzhou <zhouzemin@pingcap.com>
2022-09-16 00:19:32 +0800

    .
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- cdc/scripts/codecov-integration-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -Os https://uploader.codecov.io/latest/linux/codecov
-chmod +x codecov
-./codecov -F cdc-integration-test -s /tmp/tikv_cdc_test -f '*.out' -t $TIKV_MIGRATION_CODECOV_TOKEN
-rm -rf codecov
