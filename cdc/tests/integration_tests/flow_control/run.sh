bc3552956b479eccaa30dd918eff00693766e8a5
zeminzhou <zhouzemin@pingcap.com>
2022-10-12 19:40:04 +0800

    10w -> 50w
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- cdc/tests/integration_tests/flow_control/run.sh
+++ cdc/tests/integration_tests/flow_control/run.sh
@@ -30,10 +30,10 @@ function run() {
 
 	tikv-cdc cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
 
-	rawkv_op $UP_PD put 100000 #10w
+	rawkv_op $UP_PD put 500000 #10w
 
 	check_sync_diff $WORK_DIR $UP_PD $DOWN_PD
-	rawkv_op $UP_PD delete 100000
+	rawkv_op $UP_PD delete 500000
 	check_sync_diff $WORK_DIR $UP_PD $DOWN_PD
 
 	cleanup_process $CDC_BINARY
