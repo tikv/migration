86a1f7e320ee253f7430c198d80e0934acbce9c0
zeminzhou <zhouzemin@pingcap.com>
2022-10-21 16:22:49 +0800

    show diff
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- cdc/tests/.unmodify/kill_owner_with_ddl/run.sh
+++ cdc/tests/.unmodify/kill_owner_with_ddl/run.sh
@@ -5,15 +5,17 @@ set -eu
 CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $CUR/../_utils/test_prepare
 WORK_DIR=$OUT_DIR/$TEST_NAME
-CDC_BINARY=cdc.test
+CDC_BINARY=tikv-cdc.test
 SINK_TYPE=$1
 
 MAX_RETRIES=10
+UP_PD=http://$UP_PD_HOST_1:$UP_PD_PORT_1
+DOWN_PD=http://$DOWN_PD_HOST:$DOWN_PD_PORT
 
 function check_capture_count() {
 	pd=$1
 	expected=$2
-	count=$(cdc cli capture list --pd=$pd 2>&1 | jq '.|length')
+	count=$(tikv-cdc cli capture list --pd=$pd 2>&1 | jq '.|length')
 	if [[ ! "$count" -eq "$expected" ]]; then
 		echo "count: $count expected: $expected"
 		exit 1
@@ -25,12 +27,12 @@ function kill_cdc_and_restart() {
 	work_dir=$2
 	cdc_binary=$3
 	MAX_RETRIES=10
-	status=$(curl -s http://127.0.0.1:8300/status)
+	status=$(curl -s http://127.0.0.1:8600/status)
 	cdc_pid=$(echo "$status" | jq '.pid')
 
 	kill $cdc_pid
 	ensure $MAX_RETRIES check_capture_count $pd_addr 0
-	run_cdc_server --workdir $work_dir --binary $cdc_binary --addr "127.0.0.1:8300" --pd $pd_addr
+	run_cdc_server --workdir $work_dir --binary $cdc_binary --addr "127.0.0.1:8600" --pd $pd_addr
 	ensure $MAX_RETRIES check_capture_count $pd_addr 1
 }
 
@@ -38,49 +40,36 @@ export -f check_capture_count
 export -f kill_cdc_and_restart
 
 function run() {
-	# kafka is not supported yet.
-	if [ "$SINK_TYPE" == "kafka" ]; then
-		return
-	fi
 
 	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 	start_tidb_cluster --workdir $WORK_DIR
 	cd $WORK_DIR
 
-	pd_addr="http://$UP_PD_HOST_1:$UP_PD_PORT_1"
-	SINK_URI="mysql://normal:123456@127.0.0.1:3306/?max-txn-row=1"
+	case $SINK_TYPE in
+	tikv) SINK_URI="tikv://${DOWN_PD_HOST}:${DOWN_PD_PORT}" ;;
+	*) SINK_URI="" ;;
+	esac
 
-	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8300" --pd $pd_addr
-	cdc cli changefeed create --pd=$pd_addr --sink-uri="$SINK_URI"
-	run_sql "CREATE DATABASE kill_owner_with_ddl;" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-	run_sql "CREATE table kill_owner_with_ddl.t1 (id int primary key auto_increment, val int);" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-	check_table_exists "kill_owner_with_ddl.t1" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --addr "127.0.0.1:8600" --pd $UP_PD
+    tikv-cdc cli changefeed create --pd=$UP_PD --sink-uri="$SINK_URI"
+    sleep 10
 
-	export GO_FAILPOINTS='github.com/tikv/migration/cdc/cdc/sink/MySQLSinkExecDDLDelay=return(true);github.com/tikv/migration/cdc/cdc/capture/ownerFlushIntervalInject=return(10)'
-	kill_cdc_and_restart $pd_addr $WORK_DIR $CDC_BINARY
+	export GO_FAILPOINTS='github.com/tikv/migration/cdc/cdc/capture/ownerFlushIntervalInject=return(10)'
+	kill_cdc_and_restart $UP_PD $WORK_DIR $CDC_BINARY
 
-	for i in $(seq 2 3); do
-		run_sql "CREATE table kill_owner_with_ddl.t$i (id int primary key auto_increment, val int);" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-	done
-	for i in $(seq 1 3); do
-		run_sql "INSERT INTO kill_owner_with_ddl.t$i VALUES (),(),();" ${UP_TIDB_HOST} ${UP_TIDB_PORT}
-	done
-
-	# sleep to ensure processor has consumed the DDL and flushed checkpoint
-	sleep 5
+	rawkv_op $UP_PD put 10000
 
 	for i in $(seq 1 3); do
-		kill_cdc_and_restart $pd_addr $WORK_DIR $CDC_BINARY
+		kill_cdc_and_restart $UP_PD $WORK_DIR $CDC_BINARY
 		sleep 8
 	done
 
 	export GO_FAILPOINTS=''
-	kill_cdc_and_restart $pd_addr $WORK_DIR $CDC_BINARY
+	kill_cdc_and_restart $UP_PD $WORK_DIR $CDC_BINARY
 
-	for i in $(seq 1 3); do
-		check_table_exists "kill_owner_with_ddl.t$i" ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
-	done
-	check_sync_diff $WORK_DIR $CUR/conf/diff_config.toml
+	check_sync_diff $WORK_DIR $UP_PD $DOWN_PD
+	rawkv_op $UP_PD delete 10000
+	check_sync_diff $WORK_DIR $UP_PD $DOWN_PD
 
 	cleanup_process $CDC_BINARY
 }
--- cdc/tests/.unmodify/processor_panic/run.sh
+++ cdc/tests/.unmodify/processor_panic/run.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
-set -eu
+set -euo pipefail
 
 CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source $CUR/../_utils/test_prepare
 WORK_DIR=$OUT_DIR/$TEST_NAME
-CDC_BINARY=cdc.test
+CDC_BINARY=tikv-cdc.test
 SINK_TYPE=$1
+UP_PD=http://$UP_PD_HOST_1:$UP_PD_PORT_1
+DOWN_PD=http://$DOWN_PD_HOST:$DOWN_PD_PORT
 
-function prepare() {
+function run() {
 	rm -rf $WORK_DIR && mkdir -p $WORK_DIR
 
 	start_tidb_cluster --workdir $WORK_DIR
 
 	cd $WORK_DIR
 
-	# record tso before we create tables to skip the system table DDLs
-	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
-
-	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix 1 --addr 127.0.0.1:8300 --restart true \
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix 1 --addr 127.0.0.1:8600 --restart true \
 		--failpoint 'github.com/tikv/migration/cdc/cdc/processor/pipeline/ProcessorSyncResolvedPreEmit=return(true)'
 
-	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix 2 --addr 127.0.0.1:8301
-
-	TOPIC_NAME="ticdc-processor-panic-test-$RANDOM"
 	case $SINK_TYPE in
-	kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&kafka-client-id=cdc_test_processor_panic&kafka-version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
-	*) SINK_URI="mysql://normal:123456@127.0.0.1:3306/" ;;
+	tikv) SINK_URI="tikv://${DOWN_PD_HOST}:${DOWN_PD_PORT}" ;;
+	*) SINK_URI="" ;;
 	esac
-	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
-	if [ "$SINK_TYPE" == "kafka" ]; then
-		run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&version=${KAFKA_VERSION}&max-message-bytes=10485760"
-	fi
+    # Make sure the task is assigned to the first cdc
+	run_cdc_cli changefeed create --sink-uri="$SINK_URI"
+    sleep 10
+
+	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY --logsuffix 2 --addr 127.0.0.1:8601
+
+	rawkv_op $UP_PD put 10000
+	check_sync_diff $WORK_DIR $UP_PD $DOWN_PD
+	rawkv_op $UP_PD delete 10000
+	check_sync_diff $WORK_DIR $UP_PD $DOWN_PD
+
+    cleanup_process $CDC_BINARY
 }
 
 trap stop_tidb_cluster EXIT
-prepare $*
+run $*
 
-cd "$(dirname "$0")"
-set -o pipefail
-GO111MODULE=on go run main.go -config ./config.toml 2>&1 | tee $WORK_DIR/tester.log
-check_table_exists test.end_mark_table ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT} 90
-check_sync_diff $WORK_DIR $CUR/diff_config.toml
-cleanup_process $CDC_BINARY
 check_logs $WORK_DIR
 echo "[$(date)] <<<<<< run test case $TEST_NAME success! >>>>>>"
